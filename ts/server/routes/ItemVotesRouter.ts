b406fb7a5db406daea668a8fc60cce329407ddbc
brugnara <brugnara@spaziodati.eu>
2021-10-23 23:34:19 +0200

    celanup

--- ts/server/routes/ItemVotesRouter.ts
+++ ts/server/routes/ItemVotesRouter.ts
@@ -1,8 +1,5 @@
-import FalcorJsonGraph from 'falcor-json-graph';
-import FalcorRouter from 'falcor-router';
 import { FindOptions } from 'mongodb';
 import { ItemVoteFromDB } from '../../common/Types';
-import { Prefixer } from '../helpers/Prefixer';
 import { Projecter } from '../helpers/Projecter';
 import { BaseRouter } from './BaseRouter';
 
@@ -24,37 +21,5 @@ export class ItemVotesRouter extends BaseRouter<ItemVoteFromDB> {
                 { $group: { _id: '$itemId', voteCount: { $sum: 1 } } },
             ])
             .toArray();
-    }
-
-    _byID(): FalcorRouter.RouteDefinition {
-        const that = this;
-
-        return {
-            route: Prefixer.byID(this.prefix),
-            async get(pathSet: FalcorJsonGraph.PathSet) {
-                const ids = pathSet.pop() as Array<string>,
-                    values = await that.collection
-                        .aggregate([
-                            { $match: { itemId: { $in: ids } } },
-                            { $group: { _id: '$itemId', count: { $sum: 1 } } },
-                        ])
-                        .toArray();
-
-                const ret = [];
-
-                ids.forEach((id) => {
-                    const value = values.find((value) => value._id === id)?.count ?? 0;
-
-                    ret.push({
-                        value,
-                        path: [...pathSet, id],
-                    });
-                });
-
-                console.log(ret);
-
-                return ret;
-            },
-        };
     }
 }
